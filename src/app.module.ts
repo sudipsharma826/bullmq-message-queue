4cdc2956939288fe6f2606d2885b316b1244b11c
Sudip Sharma <sudpokhara826@gmail.com>
2026-05-03 16:43:06 +0545

    seeting up the bullmq, redis and docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
-  imports: [],
+  imports: [BullModule.forRoot({
+    connection: {
+      host: 'localhost',
+      port: 6379,
+    },
+    defaultJobOptions:{
+      attempts:3,
+      backoff: {
+        type: 'exponential',
+        delay: 1000,
+      },
+      delay: 5000,
+    }
+  }),
+  BullModule.registerQueue({
+    name: 'job-queue',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
